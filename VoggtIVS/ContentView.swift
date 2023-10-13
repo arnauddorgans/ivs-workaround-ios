a43b2297fb85fc54028480cf8a09125f7354b4dc
Arnaud Dorgans <arnaud.dorgans@voggt.com>
2023-10-14 01:04:56 +0200

    Initial Commit

---
+++ VoggtIVS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoggtIVS
+//
+//  Created by Arnaud Dorgans on 14/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoggtIVS/VoggtIVSApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoggtIVSApp.swift
+//  VoggtIVS
+//
+//  Created by Arnaud Dorgans on 14/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct VoggtIVSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
